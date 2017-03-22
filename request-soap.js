a2924bf631c88206856dad873473d4435eaf682d
chrisjstevenson <chris.j.stevenson@gmail.com>
2017-03-22 12:22:44 -0500

    initial commit

---
+++ request-soap.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const config = require('./config.js');
+
+const pfxFile = path.resolve(__dirname,'certificates/keystore.pfx');
+
+var options = {
+    //proxy: 'http://localhost:8888', // send to charles for http-debugging, will result in mac verfiy failure if able to pass through gateway
+    strictSSL: false,  // ignore any self-signed certs in the chain, for example a certain proxy
+    pfx: fs.readFileSync(pfxFile),
+    passphrase: config.passphrase,
+    method: 'POST',
+    url: `https://${config.server.url}`,
+    qs: { wsdl: '' },
+    headers: { 'content-type': 'text/xml' },
+    body: config.soapBody };
+
+request(options, (error, response, body) => {
+    if (error) throw new Error(error);
+    console.log(body);
+});
